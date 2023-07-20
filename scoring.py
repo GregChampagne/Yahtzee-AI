87fb43e802a062f3da3ebe68200afc94dc16e58d
Greg Champagne <37176149+GregChampagne@users.noreply.github.com>
2023-07-20 02:18:10 -0500

    0.2

--- scoring.py
+++ scoring.py
@@ -41,7 +41,7 @@ class Score:
         print("4. Fours " + str(self.upper_scores[3]) + " 10. Small " + str(self.lower_scores[3]))
         print("5. Fives " + str(self.upper_scores[4]) + " 11. Large " + str(self.lower_scores[4]))
         print("6. Sixes " + str(self.upper_scores[5]) + " 12. Chance " + str(self.lower_scores[5]))
-        print("0. Yahtzee! " + str(self.yahtzee_score))
+        print("13. Yahtzee! " + str(self.yahtzee_score))
 
     def score_upper(self, c, d):
         subscore = 0
@@ -52,11 +52,99 @@ class Score:
         self.upper_scores[c - 1] = subscore
         self.print_options()
 
-
-
     def score_lower(self, c, d):
-        print("x")
+        counts = [0, 0, 0, 0, 0, 0]
+        num_sum = 0
+        max_of_kind = 1
+        for i in range(0, 5):
+            if d[i] == 1:
+                counts[0] += 1
+            elif d[i] == 2:
+                counts[1] += 1
+            elif d[i] == 3:
+                counts[2] += 1
+            elif d[i] == 4:
+                counts[3] += 1
+            elif d[i] == 5:
+                counts[4] += 1
+            elif d[i] == 6:
+                counts[5] += 1
+            num_sum += d[i]
+
+        for i in range(0, 5):
+            if counts[i] > max_of_kind:
+                max_of_kind = counts[i]
+
+        # Three of a Kind
+        if c == 0:
+            if self.lower_scores[c] == -1:
+                if max_of_kind >= 3:
+                    self.lower_scores[c] = num_sum
+                else:
+                    self.lower_scores[c] = 0
+
+        # Four of a Kind
+        if c == 1:
+            if self.lower_scores[c] == -1:
+                if max_of_kind >= 4:
+                    self.lower_scores[c] = num_sum
+                else:
+                    self.lower_scores[c] = 0
+
+        # Full House
+        if c == 2:
+            full_house = False
+            if self.lower_scores[c] == -1:
+                if max_of_kind == 3:
+
+                    for i in range(0, 5):
+                        if counts[i] == 2:
+                            full_house = True
+
+                if full_house:
+                    self.lower_scores[c] = 25
+                else:
+                    self.lower_scores[c] = 0
+
+        # Small Straight
+        if c == 3:
+            score = 0
+            if self.lower_scores[c] == -1:
+                if counts[0] > 0 and counts[1] > 0 and counts[2] > 0 and counts[3] > 0:
+                    score = 30
+                elif counts[1] > 0 and counts[2] > 0 and counts[3] > 0 and counts[4] > 0:
+                    score = 30
+                elif counts[2] > 0 and counts[3] > 0 and counts[4] > 0 and counts[5] > 0:
+                    score = 30
+                self.lower_scores[c] = score
+
+        # Large Straight
+        if c == 4:
+            score = 40
+            if self.lower_scores[c] == -1:
+                for i in range(1, 5):
+                    if d[i] != (d[i-1] + 1):
+                        score = 0
+
+                self.lower_scores[c] = score
+
+        # Chance
+        if c == 5:
+            if self.lower_scores[c] == -1:
+                self.lower_scores[c] = num_sum
 
     def score_yahtzee(self, d):
-        print('x')
+        num = d[0]
+        yahtzee = True
+        for i in range(1, 5):
+            if d[i] != num:
+                yahtzee = False
+
+        if yahtzee:
+            if self.yahtzee_score == -1:
+                self.yahtzee_score = 50
+            else:
+                self.yahtzee_score += 100
+        else:
+            self.yahtzee_score = 0
 
--- yahtzee.py
+++ yahtzee.py
@@ -8,13 +8,23 @@ more = True
 s = scoring.Score()
 
 
+# Roll the 5 dice, then ask nicely for them to be sorted
 def take_turn():
     for i in range(0, 5):
         if protect[i] == 0:
             dice[i] = random.randint(1, 6)
+    sort_dice()
     print(dice)
 
 
+# Sort the dice and set the die array to the sorted version
+def sort_dice():
+    dice_temp = sorted(dice)
+    for i in range(0, 5):
+        dice[i] = dice_temp[i]
+
+
+# Choose whether or not to protect each die
 def choose_dice():
     print("0 For no, 1 for Yes")
     for i in range(0, 5):
@@ -22,23 +32,28 @@ def choose_dice():
         protect[i] = choice
 
 
+# Handles the choice of which option to fill out, with some protections
 def decision():
     s.print_options()
     correct_choice = False
     while not correct_choice:
         choice = int(input("Pick an open slot! "))
         if choice < 7:
-            if s.lower_scores[choice - 1] == -1:
+            if s.upper_scores[choice - 1] == -1:
                 correct_choice = True
                 s.score_upper(choice, dice)
         elif choice < 13:
-            if s.upper_scores[choice - 7] == -1:
+            if s.lower_scores[choice - 7] == -1:
                 correct_choice = True
                 s.score_lower(choice - 7, dice)
         else:
             if s.yahtzee_score == -1:
+                correct_choice = True
                 s.score_yahtzee(dice)
+        for i in range(0, 5):
+            protect[i] = 0
 
+# Main Game loop
 while more:
     take_turn()
     rolls += 1
@@ -47,3 +62,4 @@ while more:
     else:
         decision()
         rolls = 0
+        s.print_options()
